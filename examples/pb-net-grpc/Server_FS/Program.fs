a5b304a87afff26d2d0e4138d221fe064a125fb7
Isaac Abraham <isaac.abraham@gmail.com>
2019-07-24 09:49:01 +0200

    Incorporate Saturn with custom keywords

--- examples/pb-net-grpc/Server_FS/Program.fs
+++ examples/pb-net-grpc/Server_FS/Program.fs
@@ -1,17 +1,40 @@
 open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Server.Kestrel.Core
+open ProtoBuf.Grpc.Server
+open Saturn
+open SaturnExtensions
 open Server_FS
 
-let createHostBuilder args =
-    WebHost.CreateDefaultBuilder(args)
-           .ConfigureKestrel(fun options ->
-                options.ListenLocalhost(10042, fun listenOptions ->
-                    listenOptions.Protocols <- HttpProtocols.Http2
-                ))
-           .UseStartup<Startup>()
+/// Add Code First GRPC
+let configureServices (services: IServiceCollection) =
+    services.AddCodeFirstGrpc()
+    services
 
-[<EntryPoint>]
-let main args =
-    createHostBuilder(args).Build().Run()
-    0
+/// Configure the host with basic listening options
+let configureHost (webHostBuilder:IWebHostBuilder) =
+    webHostBuilder
+       .ConfigureKestrel(fun options ->
+            options.ListenLocalhost(10042, fun listenOptions ->
+            listenOptions.Protocols <- HttpProtocols.Http2))
+
+/// Configures the GRPC endpoints
+let configureEndpoints (endpoints:Routing.IEndpointRouteBuilder) =
+    endpoints.MapGrpcService<MyCalculator>() |> ignore
+
+/// This is a hack and shouldn't be required.
+let routes = router { get "/" (Giraffe.ResponseWriters.text "Hello") }
+
+let app = application {
+    url "http://localhost"
+    use_router routes
+    host_config configureHost
+    service_config configureServices
+    use_developer_exceptions
+    use_routing
+    endpoint_config configureEndpoints
+}
+
+run app
---
+++ examples/pb-net-grpc/Server_FS/SaturnHelpers.fs
@@ -0,0 +1,42 @@
+module SaturnExtensions
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+type Saturn.Application.ApplicationBuilder with
+    /// Provides overrides for using custom endpoints.
+    [<CustomOperation "endpoint_config">]
+    member __.UseEndpoints(state:Saturn.Application.ApplicationState, endpointConfig) =
+        { state with
+            AppConfigs =
+                fun app -> app.UseEndpoints (fun endpoint -> endpointConfig endpoint)
+                :: state.AppConfigs }
+
+    /// Turns on basic routing.
+    [<CustomOperation "use_routing">]
+    member __.UseRouting (state:Saturn.Application.ApplicationState) =
+        { state with
+            AppConfigs =
+                fun app -> app.UseRouting()
+                :: state.AppConfigs }
+
+    /// Provides a hook for working with the web host environment.
+    [<CustomOperation "webhost_config">]
+    member __.WebHostEnvironmentConfig (state:Saturn.Application.ApplicationState, config) =
+        { state with
+            AppConfigs =
+                fun app ->
+                    let env = app.ApplicationServices.GetService<IWebHostEnvironment>()
+                    config app env
+                :: state.AppConfigs }
+
+    /// Turns on the developer exception page, if the environment is in development mode.
+    [<CustomOperation "use_developer_exceptions">]
+    member this.ActivateDeveloperExceptions (state:Saturn.Application.ApplicationState) =
+        let config (app:IApplicationBuilder) (env:IWebHostEnvironment) =
+            if env.IsDevelopment() then app.UseDeveloperExceptionPage()
+            else app
+        this.WebHostEnvironmentConfig(state, config)
+
